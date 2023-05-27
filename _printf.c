7f585ee19613c2418ecf0e657f0870f349287485
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-05-27 13:44:03 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * _print_char - Print a character and update the count
+ * @args: va_list with the arguments
+ * @count: current count of characters printed
+ */
+void _print_char(va_list args, int *count)
+{
+	int c = va_arg(args, int);
+
+	putchar(c);
+	(*count)++;
+}
+
+/**
+ * _print_string - Print a string and update the count
+ * @args: va_list with the arguments
+ * @count: current count of characters printed
+ */
+void _print_string(va_list args, int *count)
+{
+	char *s = va_arg(args, char *);
+
+	while (*s != '\0')
+	{
+		putchar(*s);
+		s++;
+		(*count)++;
+	}
+}
 
 /**
  * _printf - Printf function
@@ -20,26 +51,12 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			if (*format == '\0')
-			break;
+				break;
 
 			if (*format == 'c')
-			{
-				int c = va_arg(args, int);
-
-				putchar(c);
-				count++;
-			}
-		else if (*format == 's')
-		{
-			char *s = va_arg(args, char *);
-
-			while (*s != '\0')
-			{
-				putchar(*s);
-				s++;
-				count++;
-			}
-		}
+			_print_char(args, &count);
+			else if (*format == 's')
+			_print_string(args, &count);
 			else if (*format == '%')
 			{
 				putchar('%');
@@ -52,7 +69,7 @@ int _printf(const char *format, ...)
 			count++;
 		}
 
-		format++;
+	format++;
 	}
 
 	va_end(args);
